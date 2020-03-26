6c0cdbefa3ba467003c4c0c7fbfe1d0687c4754d
[Lacerda53] <[guilhermeandrade2013@gmail.com]>
2020-03-26 02:08:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { FiTrash2, FiPlus } from 'react-icons/fi';
 import './App.css';
 
 function App() {
+  const [tarefa, setTarefa] = useState('');
+  const [completa, setCompleta] = useState(false);
+  const [lista, setLista] = useState([]);
+
+
+  function onSubmit(event) {
+    event.preventDefault();
+    setLista([...lista, tarefa]);
+    console.log(lista.tarefa);
+  }
+
+  function isComplete(event) {
+    
+  }
+
+  function remove(item) {
+     setLista(lista.filter(x=>x != item))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <h1>TO DO React</h1>
+        <form onSubmit={onSubmit}>
+          <input placeholder="Tarefa..."
+            className="task"
+            value={tarefa}
+            onChange={(e)=> setTarefa(e.target.value)} />
+          <button class="addBtn" ><FiPlus size={14}/> Adicionar</button>
+        </form>
+      </div>
+
+      <ul>
+        {lista.map(item =>
+          <li onClick={isComplete}>
+            <input type="checkbox" className="check"/>
+            {item}
+            <span onClick={() => remove(item)} className="close"><FiTrash2 /></span>
+          </li>
+        )}
+      </ul>
     </div>
   );
 }
